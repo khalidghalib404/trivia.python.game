6bee7d6e179bfe383ff4ba9ccd97f2988f217014
Khalid ghlaib <khalidghalib426@gmail.com>
2026-05-25 22:15:05 +0430

    adding lists

---
+++ TodoList.py
@@ -0,0 +1,54 @@
+# load existing data or items 
+# 1. creating a new item 
+# 2. mark the item as complete
+# 3. save item
+# 4. delete item
+
+
+import json
+ 
+file_name = "todo_list.json"
+
+def load_tasks():
+    pass
+
+
+def save_tasks():
+    pass
+
+def view_tasks():
+    pass
+
+def create_task():
+    pass
+
+def mark_task_complete():
+    pass
+
+def main():
+    tasks = load_tasks()
+    
+    
+    while True:
+        print("\nTo-Do List Manager")
+        print("1. View Tasks")
+        print("2. Add Task")
+        print("3. Mark Task as Complete")
+        print("4. Exit")
+        
+        choice = input("Enter your choice: ").strip()
+        
+        if choice == "1":
+            view_tasks(tasks)
+        elif choice == "2":
+            create_task(tasks)
+        elif choice == "3":
+            mark_task_complete(tasks)
+        elif choice == "4":
+            print("good bye")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+            
+     
+main()
