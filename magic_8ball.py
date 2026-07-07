e1f7099558222e92f4f4ba6a309c6c9b945cdcf0
Khalid ghlaib <khalidghalib426@gmail.com>
2026-07-07 22:47:27 +0430

    final commit.

--- magic_8ball.py
+++ magic_8ball.py
@@ -7,7 +7,7 @@ def magic_8ball():
         "Outlook good 🎯", "Yes 🎉", "Signs point to yes 🌈",
         "Reply hazy, try again 🤔", "Ask again later ⏳", "Better not tell you now 🤫",
         "Cannot predict now 🌀", "Don't count on it 💔", "My reply is no 🚫",
-        "No ❌", "Very doubtful 😔", "Not in your favor. 🎲"
+        "No. ❌", "Very doubtful 😔", "Not in your favor. 🎲"
     ]
     
     print("\n" + "="*45)
